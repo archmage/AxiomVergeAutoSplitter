69e1011f00dcd6563e6af9071449ca0ce550ac3a
Christopher D Couture <videogameroulette@yahoo.ca>
2021-04-28 19:27:42 -0400

    update 1.0.0.1

--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,94 @@ var splits = document.getElementById("splits");
 var igt = document.getElementById("igt");
 var pb = document.getElementById("pb");
 
+const ProgressionItems = [
+	"AddressDisruptor1",
+	"AddressDisruptor2",
+	"BlackCoat",
+	"BreachSuppressor",
+	"Drill",
+	"DroneGun",
+	"DroneTeleport",
+	"EnhancedLaunch",
+	"GlitchBomb",
+	"GlitchTeleport",
+	"Grapple",
+	"HighJump",
+	"PasswordTool",
+	"RedCoat"
+];
+
+const Bosses = {
+	Xedur: "Xedur",
+	Telal: "Telal",
+	Uruku: "Uruku",
+	GirTab: "Gir-Tab",
+	Vision: "Vision",
+	Clone: "Clone",
+	Ukhu: "Ukhu",
+	XedurHul: "Xedur Hul",
+	End: "Athetos",
+}
+
+const NameDictionary = {
+	FirstDeath: "First Death",
+	AddressDisruptor1: "Address Disruptor",
+	AddressDisruptor2: "Address Disruptor 2",
+	BlackCoat: "Trenchcoat",
+	BreachSuppressor: "Sudran Key",
+	DataDisruptor: "Axiom Disruptor",
+	DataGrenade: "Data Bomb",
+	DistortionField: "Distortion Field",
+	Drill: "Drill",
+	DroneGun: "Remote Drone",
+	DroneTeleport: "Drone Teleport",
+	EnhancedLaunch: "Enhanced Drone Launch",
+	FatBeam: "Fat Beam",
+	FireWall: "Fire Wall",
+	FlameThrower: "Flamethrower",
+	GlitchBomb: "Address Bomb",
+	GlitchTeleport: "Modified Lab Coat",
+	Grapple: "Grapple",
+	HeatSeeker: "Heat Seeker",
+	HighJump: "Field Disruptor",
+	InertialPulse: "Inertial Pulse",
+	IonBeam: "Ion Beam",
+	Kilver: "Kilver",
+	LightningGun: "Lightning Gun",
+	MultiDisruptor: "Multi Disruptor",
+	Nova: "Nova",
+	PasswordTool: "Password Tool",
+	RedCoat: "Red Coat",
+	Reflect: "Reflector",
+	Scythe: "Reverse Slicer",
+	Shards: "Shards",
+	Swim: "Quantum Variegator",
+	TendrilsBottom: "Bioflux Accelerator Bottom",
+	TendrilsTop: "Bioflux Accelerator Top",
+	TetheredCharge: "Tethered Charge",
+	TriCone: "Turbine Pulse",
+	VerticalSpread: "Hypo-Atomizer",
+	Voranj: "Voranj",
+	WallTrace: "Orbital Discharge",
+	WebSlicer: "Scissor Beam",
+}
+
+const Difficulty = [
+	"Normal",
+	"Hard"
+];
+
+const Progression = [
+	"Default",
+	"Advanced",
+	"Masochist"
+];
+
 window.onload = function () {
 	const queryString = window.location.search;
 	const urlParams = new URLSearchParams(queryString);
 	const category = urlParams.get('category');
-	console.log(category);
+	console.log(title, category);
 	if (category != null) {
 		document.getElementById("category").innerHTML = category;
 	}
@@ -16,13 +99,44 @@ window.onload = function () {
 	socket.onmessage = (event) => appendData(JSON.parse(event.data));
 };
 
+function SetTitle(data) {
+	if (data.IsRandomizer) {
+		document.getElementsByClassName("title")[0].innerHTML = "Axiom Verge Randomizer";
+		document.getElementById("category").innerHTML = `${Difficulty[data.Difficulty]} ${Progression[data.Progression]} ${data.Seed}`;
+		return;
+	}
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	const category = urlParams.get('category');
+	document.getElementsByClassName("title")[0].innerHTML = "Axiom Verge";
+	if (category == null) {
+		document.getElementById("category").innerHTML = `${Difficulty[data.Difficulty]} Any%`;
+		return;
+	}
+	document.getElementById("category").innerHTML = `${Difficulty[data.Difficulty]} ${category}`;
+	
+}
+
 function appendData(data) {
 	if (data.AreaName == null) {
 		return;
 	}
 	ClearAll();
 	console.log(data);
+	SetTitle(data);
 	data.SplitsNames.map(split => {
+		if (split in Bosses) {
+			splits.innerHTML += `<div class="row"><img src="images/${split}.svg"/><div class="name boss">${Bosses[split]}</div><div class="split">${IGTFormattedString(data.Splits[split])}</div></div>`;
+			if (split == "End") {
+				igt.innerHTML = IGTFormattedString(data.Splits[split]);
+			}
+			return;
+		}
+		if (split in ProgressionItems)
+		{
+			splits.innerHTML += `<div class="row"><img src="images/${split}.svg"/><div class="name progression">✩ ${ProgressionItems[split]}</div><div class="split progression">${IGTFormattedString(data.Splits[split])}</div></div>`
+			return
+		}
 		if (split.includes("Note")) {
 			splits.innerHTML += `<div class="row"><img src="images/DigitalPaper.svg"/><div class="name">${split}</div><div class="split">${IGTFormattedString(data.Splits[split])}</div></div>`;
 			return;
@@ -55,44 +169,7 @@ function appendData(data) {
 			splits.innerHTML += `<div class="row"><img src="images/RangeNode.svg"/><div class="name">${split}</div><div class="split">${IGTFormattedString(data.Splits[split])}</div></div>`;
 			return;
 		}
-		if (split == "SecurityWorm") {
-			splits.innerHTML += `<div class="row"><img src="images/Xedur.svg"/><div class="name">Xedur</div><div class="split">${IGTFormattedString(data.Splits[split])}</div></div>`;
-			return;
-		}
-		if (split == "SoldierBoss") {
-			splits.innerHTML += `<div class="row"><img src="images/Telal.svg"/><div class="name">Telal</div><div class="split">${IGTFormattedString(data.Splits[split])}</div></div>`;
-			return;
-		}
-		if (split == "SlugBoss") {
-			splits.innerHTML += `<div class="row"><img src="images/Uruku.svg"/><div class="name">Uruku</div><div class="split">${IGTFormattedString(data.Splits[split])}</div></div>`;
-			return;
-		}
-		if (split == "ScorpionBoss") {
-			splits.innerHTML += `<div class="row"><img src="images/GirTab.svg"/><div class="name">Gir-Tab</div><div class="split">${IGTFormattedString(data.Splits[split])}</div></div>`;
-			return;
-		}
-		if (split == "MantaBoss") {
-			splits.innerHTML += `<div class="row"><img src="images/Vision.svg"/><div class="name">Vision</div><div class="split">${IGTFormattedString(data.Splits[split])}</div></div>`;
-			return;
-		}
-		if (split == "DeformedTrace") {
-			splits.innerHTML += `<div class="row"><img src="images/Clone.svg"/><div class="name">Clone</div><div class="split">${IGTFormattedString(data.Splits[split])}</div></div>`;
-			return;
-		}
-		if (split == "SpitBugBoss") {
-			splits.innerHTML += `<div class="row"><img src="images/Ukhu.svg"/><div class="name">Ukhu</div><div class="split">${IGTFormattedString(data.Splits[split])}</div></div>`;
-			return;
-		}
-		if (split == "SecurityWormAdvanced")
-		{
-			splits.innerHTML += `<div class="row"><img src="images/Xedur.svg"/><div class="name">Xedur Hul</div><div class="split">${IGTFormattedString(data.Splits[split])}</div></div>`;
-			return;
-		}
-		if (split == "End") {
-			igt.innerHTML = IGTFormattedString(data.Splits["End"]);
-			return;
-		}
-		splits.innerHTML += `<div class="row"><img src="images/${split}.svg"/><div class="name">${split}</div><div class="split">${IGTFormattedString(data.Splits[split])}</div></div>`;
+		splits.innerHTML += `<div class="row"><img src="images/${split}.svg"/><div class="name">${NameDictionary[split]}</div><div class="split">${IGTFormattedString(data.Splits[split])}</div></div>`;
 	});
 	pb.innerHTML = IGTFormattedString(data.PersonalBest);
 	document.getElementById("splits").scrollTop = document.getElementById("splits").scrollHeight;
